a0f7276ab050936e06205841aba2d3d394fbdacc
guille-alonso <guilloalonsot@gmail.com>
2024-01-19 12:32:03 -0300

    cambios en auth + eficiencia

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,14 +8,14 @@ const auth = async (req, res, next) => {
     const { id } = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.id = id;
 
-    const connection = await conectarBDUsuariosMySql();
-    const [result] = await connection.execute(
-      'SELECT * FROM usuario WHERE id = ?',
-      [id]
-    );
+    // const connection = await conectarBDUsuariosMySql();
+    // const [result] = await connection.execute(
+    //   'SELECT * FROM usuario WHERE id = ?',
+    //   [id]
+    // );
 
-    req.user = result[0];
-    await connection.end();
+    // req.user = result[0];
+    // await connection.end();
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid token" });
