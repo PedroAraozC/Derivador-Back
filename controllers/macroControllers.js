f7e8aa584574f34768d81cb169cc756b3a6850f6
damianpazv <damjpv@gmail.com>
2024-04-09 12:50:11 -0300

    se creo endpoint de listar reclamos de un ciudadano

--- controllers/macroControllers.js
+++ controllers/macroControllers.js
@@ -43,6 +43,51 @@ const obtenerTiposDeReclamoPorCategoria = async (req, res) => {
     }
 }
 
-module.exports = {
-    obtenerCategorias,obtenerTiposDeReclamoPorCategoria
+const listarReclamosCiudadano= async (req, res) => {
+    const cuit = req.body.cuit;
+    const telefono = req.body.telefono;
+    const connection = await conectarMySql();
+    console.log('Conectado a MySQL');
+   
+    try {
+
+if(cuit)
+{
+    let sqlQuery = "SELECT r.id_reclamo,  tr.nombre_treclamo, r.asunto, r.direccion, r.apellido_nombre, r.fecha_hora_inicio, cr.nombre_categoria FROM reclamo_prueba r  JOIN categoria_reclamo cr ON r.id_categoria = cr.id_categoria JOIN tipo_reclamo tr ON r.id_treclamo = tr.id_treclamo WHERE r.cuit = ? ";
+    
+
+    const [reclamos] = await connection.execute(sqlQuery, [cuit]);
+    
+ await connection.end();
+
+ if(reclamos.length>0) res.status(200).json({ reclamos });
+ else res.status(200).json({ message:"no se encontraron reclamos asociados al CUIT" });
+    
+
 }
+
+if(telefono)
+{
+    let sqlQuery = "SELECT id_reclamo, id_categoria, id_treclamo, asunto, direccion, apellido_nombre, fecha_hora_inicio FROM reclamo_prueba WHERE telefono = ?";
+    
+
+    const [reclamos] = await connection.execute(sqlQuery, [telefono]);
+ await connection.end();
+
+ if(reclamos.length>0) res.status(200).json({ reclamos });
+ else res.status(200).json({ message:"no se encontraron reclamos asociados al Telefono" });
+    
+
+}
+
+
+  
+    } catch (error) {
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    }
+  }
+
+module.exports = {
+    obtenerCategorias,obtenerTiposDeReclamoPorCategoria,listarReclamosCiudadano
+}
+
