4549f5cdc99343edd2477a89f56e3392408b4761
tob-alvarez <tosal1099@gmail.com>
2024-05-07 12:43:38 -0300

    cambios para admin Licitaciones

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -4,6 +4,34 @@ const { sequelize_ciu_digital_derivador } = require("../config/sequelize");
 const Proceso = require("../models/Derivador/Proceso");
 const PermisoTUsuario = require("../models/Derivador/PermisoTUsuario");
 const PermisoPersona = require("../models/Derivador/PermisoPersona");
+const fs = require('fs');
+const path = require('path');
+
+const sobrescribirArchivo = (rutaArchivoAntiguo, rutaArchivoNuevo) => {
+  try {
+    // Leer el contenido del nuevo archivo
+    const contenidoNuevo = fs.readFileSync(rutaArchivoNuevo);
+    // Sobrescribir el archivo antiguo con el contenido del nuevo archivo
+    fs.writeFileSync(rutaArchivoAntiguo, contenidoNuevo);
+    console.log(`Archivo sobrescrito: ${rutaArchivoAntiguo}`);
+  } catch (error) {
+    console.error(`Error al sobrescribir el archivo: ${error}`);
+  }
+};
+// Función para obtener la ruta del archivo antiguo
+const obtenerRutaArchivoAntiguo = (oldName) => {
+  // Suponiendo que los archivos antiguos se guardan en una carpeta llamada 'pdfs' en el escritorio
+  const rutaArchivoAntiguo = path.join('C:/Users/Tobias/Desktop', oldName);
+  console.log(rutaArchivoAntiguo)
+  return rutaArchivoAntiguo;
+};
+
+// Función para obtener la ruta del nuevo archivo
+const obtenerRutaArchivoNuevo = (nombre_archivo) => {
+  // Suponiendo que los archivos nuevos también se guardan en una carpeta llamada 'pdfs' en el escritorio
+  const rutaArchivoNuevo = path.join('C:/Users/Tobias/Desktop', nombre_archivo);
+  return rutaArchivoNuevo;
+};
 
 const agregarOpcion = async (req, res) => {
     try {
@@ -202,6 +230,22 @@ const listarContratacionBack = async (req, res) => {
   }
 }
 
+const listarContratacion = async (req, res) => {
+  const connection = await conectarSMTContratacion();
+  try {
+    const [contrataciones] = await connection.execute(
+      'SELECT * FROM contratacion WHERE habilita = 1'
+    );
+
+    contrataciones.reverse();
+    connection.end();
+    res.status(200).json({ contrataciones })
+
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Algo salió mal :(" });
+  }
+}
+
 const agregarContratacion = async (req, res) => {
   try {
     const {
@@ -218,14 +262,14 @@ const agregarContratacion = async (req, res) => {
       habilita
     } = req.body;
 
-    // const archivo = req.file;
+    const archivo = req.file;
 
-    // if (!archivo) {
-    //   return res.status(400).json({ message: "Por favor, adjunta un archivo" });
-    // }
+    if (!archivo) {
+      return res.status(400).json({ message: "Por favor, adjunta un archivo" });
+    }
 
-    // // Obtener el nombre del archivo cargado
-    // const nombre_archivo = archivo.originalname;
+    // Obtener el nombre del archivo cargado
+    const nombre_archivo = archivo.originalname;
     
     // Obtener el último id_contratacion de la tabla
     const connection = await conectarSMTContratacion();
@@ -233,7 +277,7 @@ const agregarContratacion = async (req, res) => {
     let nextId = lastIdResult[0].max_id + 1; // Generar el próximo id_contratacion
     // Query para insertar una nueva convocatoria
     const sql =
-      "INSERT INTO contratacion (id_contratacion, fecha_apertura, hora_apertura, fecha_presentacion, hora_presentacion, nombre_contratacion, id_tcontratacion, num_instrumento, id_tinstrumento, expte, valor_pliego, habilita) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      "INSERT INTO contratacion (id_contratacion, fecha_apertura, hora_apertura, fecha_presentacion, hora_presentacion, nombre_contratacion, id_tcontratacion, num_instrumento, id_tinstrumento, expte, valor_pliego, habilita, nombre_archivo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     const values = [
       nextId,
       fecha_apertura,
@@ -247,6 +291,7 @@ const agregarContratacion = async (req, res) => {
       expte,
       valor_pliego,
       habilita,
+      nombre_archivo
     ];
 console.log(values)
     // Ejecutar la consulta SQL para insertar la nueva convocatoria
@@ -278,7 +323,48 @@ const borrarContratacion = async (req, res) => {
   }
 };
 
+const editarContratacion = async (req, res) => {
+  try {
+    const { id, nombre_contratacion, id_tcontratacion, fecha_presentacion, hora_presentacion, num_instrumento, valor_pliego, expte, id_tinstrumento, fecha_apertura, hora_apertura, habilita, oldName } = req.body;
+    // Verificar si hay un archivo adjunto
+    const archivo = req.file;
+    let nombre_archivo = null;
+    if (archivo) {
+      nombre_archivo = `CONTRATACION_${num_instrumento}_EXPTE_${expte}.pdf`;
+      console.log(req.body)
+      // Sobrescribir el archivo físico en el sistema de archivos si es necesario
+      const rutaArchivoAntiguo = obtenerRutaArchivoAntiguo(oldName); // Define esta función para obtener la ruta del archivo antiguo
+      const rutaArchivoNuevo = obtenerRutaArchivoNuevo(nombre_archivo); // Define esta función para obtener la ruta del archivo nuevo
+      sobrescribirArchivo(rutaArchivoAntiguo, rutaArchivoNuevo); // Define esta función para sobrescribir el archivo físico
+    }
+    // Query para actualizar la contratacion
+    const sql = "UPDATE contratacion SET nombre_contratacion = ?, id_tcontratacion = ?, fecha_presentacion = ?, hora_presentacion = ?, num_instrumento = ?, valor_pliego = ?, expte = ?, id_tinstrumento = ?, fecha_apertura = ?, hora_apertura = ?, habilita = ?, nombre_archivo = ? WHERE id_contratacion = ?";
+    const values = [nombre_contratacion, id_tcontratacion, fecha_presentacion, hora_presentacion, num_instrumento, valor_pliego, expte, id_tinstrumento, fecha_apertura, hora_apertura, habilita, nombre_archivo, id];
+    console.log(values)
+    // Verificar si la contratacion ya existe con otra ID
+    const connection = await conectarSMTContratacion();
+    const [contratacion] = await connection.execute(
+      "SELECT * FROM contratacion WHERE (nombre_contratacion = ? AND id_tcontratacion = ? AND num_instrumento = ? AND valor_pliego = ? AND expte = ? AND habilita = ?) AND id_contratacion != ?",
+      [nombre_contratacion,id_tcontratacion, num_instrumento, valor_pliego, expte, habilita, id ]
+    );
+
+    if (contratacion.length === 0) {
+      // No existe otra contratacion con los mismos datos, se puede proceder con la actualización
+      const [result] = await connection.execute(sql, values);
+      console.log("Filas actualizadas:", result.affectedRows);
+      res.status(200).json({ message: "Contratacion modificada con éxito", result });
+    } else {
+      // Ya existe otra contratacion con los mismos datos, devolver un error
+      res.status(400).json({
+        message: "Ya existe una contratacion con los mismos datos",
+        contratacion: contratacion[0],
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Algo salió mal :(" });
+  }
+};
 //-----------CONTRATACIONES--------------
 
 
-module.exports={ agregarOpcion, borrarOpcion, agregarProceso, listarTipoContratacion, listarTipoInstrumento, agregarContratacion, listarContratacionBack, borrarContratacion}
+module.exports={ agregarOpcion, borrarOpcion, agregarProceso, listarTipoContratacion, listarTipoInstrumento, agregarContratacion, listarContratacionBack, borrarContratacion, editarContratacion, listarContratacion}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,23 @@
 const { Router } = require("express");
 const auth = require("../middlewares/auth");
 const verifyRole = require("../middlewares/verifyRole");
-const { agregarOpcion, borrarOpcion, agregarProceso, listarTipoContratacion, listarTipoInstrumento, agregarContratacion, listarContratacion, listarContratacionBack, borrarContratacion } = require("../controllers/adminControllers");
+const { agregarOpcion, borrarOpcion, agregarProceso, listarTipoContratacion, listarTipoInstrumento, agregarContratacion, listarContratacion, listarContratacionBack, borrarContratacion, editarContratacion } = require("../controllers/adminControllers");
 
 const router = Router();
+const multer  = require('multer');
+// Configurar multer para manejar la carga de archivos
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'C:/Users/Tobias/Desktop');
+        },
+    filename: function (req, file, cb) {
+        const { num_instrumento, expte } = req.body;
+        const nombreArchivo = `CONTRATACION_${num_instrumento}_EXPTE_${expte}.pdf`;
+        cb(null, nombreArchivo);
+        }
+    });
+
+const upload = multer({ storage: storage });
 
 router.post("/altaOpcion", agregarOpcion);
 router.post("/altaProceso", agregarProceso);
@@ -13,7 +27,9 @@ router.post("/borrarOpcion", borrarOpcion);
 router.get("/listaTipoContratacion", listarTipoContratacion);
 router.get("/listarTipoIntrumentos", listarTipoInstrumento);
 router.get("/listarContratacionBack", listarContratacionBack);
-router.post("/agregarContratacion", agregarContratacion);
+router.get("/listarContratacion", listarContratacion);
+router.post("/agregarContratacion", upload.single('archivo'), agregarContratacion);
+router.put("/editarContratacion", upload.single('archivo'), editarContratacion);
 router.post("/borrarContratacion", borrarContratacion);
 //--------Contrataciones-------------
 
