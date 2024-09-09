4b23e23cc2983f21bc5c114be17cb08128a20df7
Damian <damjpv@gmail.com>
2024-09-09 12:50:23 -0300

    se crearon controladores de indicadores

--- controllers/gerenciaDatosControllers.js
+++ controllers/gerenciaDatosControllers.js
@@ -1,6 +1,43 @@
 const { conectar_BD_GED_MySql } = require("../config/dbEstadisticasMYSQL");
 
 
+
+const obtenerPerfilPorCuilGED = async (req, res) => {
+    const { cuil } = req.params;
+    const connection = await conectar_BD_GED_MySql();
+  
+    try {
+        // Obtener el perfil_id correspondiente al cuil
+        const [usuarios] = await connection.execute(
+            'SELECT perfil_id FROM usuario WHERE usuario_cuil = ?',
+            [cuil]
+        );
+  
+        if (usuarios.length === 0) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+  
+        const perfilId = usuarios[0].perfil_id;
+  
+        // Obtener la fila completa del perfil correspondiente al perfil_id
+        const [perfiles] = await connection.execute(
+            'SELECT * FROM perfil WHERE perfil_id = ?',
+            [perfilId]
+        );
+  
+        if (perfiles.length === 0) {
+            return res.status(404).json({ message: 'Perfil no encontrado' });
+        }
+  
+        res.status(200).json(perfiles[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message || 'Algo salió mal :(' });
+    } finally {
+        await connection.end();
+    }
+  };
+
 ////////// REPARTICIONES ////////////////////////////////////////////////////////////
 
 
@@ -38,6 +75,46 @@ FROM reparticion a ORDER BY reparticion_codigo
       }
     }
   };
+
+  const obtenerReparticionesFiltradasGED = async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql();
+  
+      // Obtener el cuil desde los parámetros de la solicitud
+      const { cuil } = req.params;
+  
+      // Consulta SQL para obtener las reparticiones filtradas por el cuil en la tabla permisos
+      const sqlReparticionesFiltradas = `
+        SELECT a.reparticion_id, a.reparticion_codigo, a.reparticion_det, a.reparticion_dependencia,
+        (SELECT reparticion_det FROM reparticion b WHERE b.reparticion_id = a.reparticion_dependencia) AS dependencia,
+        a.reparticion_tipo, IF(a.reparticion_tipo = 1, 'SECRETARIA', IF(a.reparticion_tipo = 2, 'SUBSECRETARIA', 'DIRECCION')) AS tipo
+        FROM reparticion a
+        JOIN permisos p ON a.reparticion_id = p.reparticion_id
+        WHERE p.usuario_cuil = ?
+        ORDER BY a.reparticion_codigo
+      `;
+  
+      // Ejecutar la consulta con el cuil proporcionado
+      const [reparticionesFiltradas] = await connection.execute(sqlReparticionesFiltradas, [cuil]);
+  
+      if (reparticionesFiltradas.length > 0) {
+        res.status(200).json({ reparticiones: reparticionesFiltradas });
+      } else {
+        res.status(204).json({ message: "No se encontraron reparticiones para este CUIL." });
+      }
+    } catch (error) {
+      // Manejo de errores
+      console.error('Error al obtener las reparticiones filtradas:', error);
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
 
   const agregarReparticionGED = async (req, res) => {
     let connection;
@@ -212,6 +289,46 @@ INNER JOIN reparticion ON programa.reparticion_id=reparticion.reparticion_id
     }
   };
 
+  const obtenerProgramasPorReparticionGED = async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql();
+  
+      // Obtener el reparticion_id desde los parámetros de la solicitud
+      const { reparticion_id } = req.params;
+  
+      // Consulta SQL para obtener los programas filtrados por reparticion_id
+      const sqlProgramasFiltrados = `
+        SELECT programa_id, programa_codigo, programa_det, programa.reparticion_id, reparticion_det,
+        reparticion_dependencia, 
+        (SELECT reparticion_det FROM reparticion b WHERE b.reparticion_id = reparticion.reparticion_dependencia) AS dependencia,
+        programa_descripcion 
+        FROM programa 
+        INNER JOIN reparticion ON programa.reparticion_id = reparticion.reparticion_id
+        WHERE programa.reparticion_id = ?
+      `;
+  
+      // Ejecutar la consulta con el reparticion_id proporcionado
+      const [programasFiltrados] = await connection.execute(sqlProgramasFiltrados, [reparticion_id]);
+  
+      if (programasFiltrados.length > 0) {
+        res.status(200).json({ programas: programasFiltrados });
+      } else {
+        res.status(204).json({ message: "No se encontraron programas para esta repartición." });
+      }
+    } catch (error) {
+      // Manejo de errores detallado
+      console.error('Error al obtener los programas filtrados:', error);
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
+
 const agregarProgramaGED = async (req, res) => {
     let connection;
     try {
@@ -347,4 +464,263 @@ const eliminarProgramaGED = async (req, res) => {
 };
 
 
-  module.exports ={obtenerReparticionesGED,agregarReparticionGED,editarReparticionGED,eliminarReparticionGED,obtenerProgramasGED,agregarProgramaGED,editarProgramaGED,eliminarProgramaGED}
+
+////////////////INDICADORES/////////////////////////
+
+const obtenerIndicadoresGED= async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql(); // Asegúrate de que esta función esté definida y se conecte correctamente a tu base de datos.
+  
+      // Consulta para obtener todos los proveedores
+      const sqlProveedores = `
+       SELECT indicador_id,indicador_det,indicador_descripcion,indicador.unidad_id,unidad_det,
+(SELECT GROUP_CONCAT(programa_det) FROM programa INNER JOIN r_indicador_programa b ON programa.programa_id=b.programa_id WHERE b.indicador_id=indicador.indicador_id) programas
+FROM indicador INNER JOIN unidad ON indicador.unidad_id=unidad.unidad_id
+      `;
+      const [indicadores] = await connection.execute(sqlProveedores);
+  
+      // Consulta para obtener todos los rubros asociados a cada proveedor
+      const sqlProveedoresRubros = `
+        SELECT r.indicador_id, r.programa_id, rb.programa_det
+        FROM r_indicador_programa r
+        LEFT JOIN programa rb ON r.programa_id = rb.programa_id
+      `;
+      const [indicadoresProgramas] = await connection.execute(sqlProveedoresRubros);
+  
+      // Enviar los resultados como respuesta
+      res.status(200).json({ indicadores, indicadoresProgramas });
+    } catch (error) {
+      // Manejo de errores detallado
+      console.error('Error al obtener los datos:', error);
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+
+  const obtenerUnidadesGED = async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql(); 
+  
+      const sql = `
+SELECT * FROM unidad
+      `;
+      const [unidades] = await connection.execute(sql);
+  
+      if (unidades)
+      {
+        res.status(200).json({ unidades });
+      }
+  
+  else {
+    res.status(204).json({ mge:"no hay datos" });
+  }
+      // Enviar los resultados como respuesta
+     
+    } catch (error) {
+      // Manejo de errores detallado
+      console.error('Error al obtener los datos:', error);
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
+  const agregarIndicadorGED = async (req, res) => {
+    let connection;
+    console.log(req.body);
+    try {
+      connection = await conectar_BD_GED_MySql();
+  
+      // Datos recibidos desde el request
+      const { indicador_det,indicador_descripcion,unidad_id } = req.body.nuevoIndicador;
+      const obj = req.body.selectedPrograma;
+      const ids = Object.values(obj);
+  
+      // Iniciar una transacción
+      await connection.beginTransaction();
+  
+      // Consulta para insertar un nuevo proveedor
+      const sqlInsertIndicador = `
+        INSERT INTO indicador (indicador_det,indicador_descripcion,unidad_id)
+        VALUES (?, ?, ?)
+      `;
+  
+      // Ejecución de la consulta con los valores a insertar
+      const [result] = await connection.execute(sqlInsertIndicador, [
+        indicador_det.toUpperCase(),
+        indicador_descripcion.toUpperCase(),
+         unidad_id
+      ]);
+  
+      // Verificar si alguna fila fue afectada (es decir, si el proveedor fue insertado)
+      if (result.affectedRows === 0) {
+        await connection.rollback(); // Deshacer la transacción en caso de error
+        return res.status(400).json({ message: "No se pudo agregar el indicador", ok: false });
+      }
+  
+      const indicadorId = result.insertId;
+  
+    
+      const sqlInsertProgramaIndicadorr = `
+        INSERT INTO r_indicador_programa (indicador_id, programa_id)
+        VALUES (?, ?)
+      `;
+  
+    
+      for (const idPrograma of ids) {
+        const [resultPrograma] = await connection.execute(sqlInsertProgramaIndicadorr, [indicadorId, idPrograma]);
+  
+   
+        if (resultPrograma.affectedRows === 0) {
+          await connection.rollback(); // Deshacer la transacción en caso de error
+          return res.status(400).json({ message: "No se pudo agregar el programa", ok: false });
+        }
+      }
+  
+      // Confirmar la transacción si todo fue exitoso
+      await connection.commit();
+  
+      res.status(201).json({ message: "Indicador y programas agregados correctamente", ok: true, id: indicadorId });
+    } catch (error) {
+      console.error('Error al agregar el indicador y los programas:', error);
+      if (connection) await connection.rollback(); // Deshacer la transacción en caso de error
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
+  const editarIndicadorGED = async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql();
+      
+      // Datos recibidos desde el request
+      const { indicador_id, indicador_det, indicador_descripcion, unidad_id } = req.body.indicadorEditado;
+      const obj = req.body.selectedPrograma;
+      const ids = Object.values(obj);
+      
+      // Iniciar una transacción
+      await connection.beginTransaction();
+  
+      // Consulta para actualizar el indicador
+      const sqlUpdateIndicador = `
+        UPDATE indicador
+        SET indicador_det = ?, indicador_descripcion = ?, unidad_id = ?
+        WHERE indicador_id = ?
+      `;
+  
+      // Ejecución de la consulta con los valores a actualizar
+      const [result] = await connection.execute(sqlUpdateIndicador, [
+        indicador_det.toUpperCase(),
+        indicador_descripcion.toUpperCase(),
+        unidad_id,
+        indicador_id
+      ]);
+  
+      // Verificar si alguna fila fue afectada
+      if (result.affectedRows === 0) {
+        await connection.rollback(); // Deshacer la transacción en caso de error
+        return res.status(400).json({ message: "No se pudo actualizar el indicador", ok: false });
+      }
+  
+      // Eliminar las relaciones anteriores entre el indicador y los programas
+      const sqlDeleteRelaciones = `
+        DELETE FROM r_indicador_programa
+        WHERE indicador_id = ?
+      `;
+      await connection.execute(sqlDeleteRelaciones, [indicador_id]);
+  
+      // Insertar las nuevas relaciones entre el indicador y los programas
+      const sqlInsertProgramaIndicador = `
+        INSERT INTO r_indicador_programa (indicador_id, programa_id)
+        VALUES (?, ?)
+      `;
+  
+      for (const idPrograma of ids) {
+        const [resultPrograma] = await connection.execute(sqlInsertProgramaIndicador, [indicador_id, idPrograma]);
+  
+        if (resultPrograma.affectedRows === 0) {
+          await connection.rollback(); // Deshacer la transacción en caso de error
+          return res.status(400).json({ message: "No se pudo actualizar las relaciones de programas", ok: false });
+        }
+      }
+  
+      // Confirmar la transacción si todo fue exitoso
+      await connection.commit();
+  
+      res.status(200).json({ message: "Indicador y programas actualizados correctamente", ok: true });
+    } catch (error) {
+      console.error('Error al actualizar el indicador y los programas:', error);
+      if (connection) await connection.rollback(); // Deshacer la transacción en caso de error
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
+  const eliminarIndicadorGED = async (req, res) => {
+    let connection;
+    try {
+      connection = await conectar_BD_GED_MySql();
+  
+      // Obtener el ID del indicador desde los parámetros de la solicitud
+      const { indicador_id } = req.params;
+  
+      // Iniciar una transacción
+      await connection.beginTransaction();
+  
+      // Eliminar las relaciones entre el indicador y los programas
+      const sqlDeleteRelaciones = `
+        DELETE FROM r_indicador_programa
+        WHERE indicador_id = ?
+      `;
+      const [resultRelaciones] = await connection.execute(sqlDeleteRelaciones, [indicador_id]);
+  
+      // Eliminar el indicador de la tabla indicador
+      const sqlDeleteIndicador = `
+        DELETE FROM indicador
+        WHERE indicador_id = ?
+      `;
+      const [resultIndicador] = await connection.execute(sqlDeleteIndicador, [indicador_id]);
+  
+      // Verificar si el indicador fue eliminado
+      if (resultIndicador.affectedRows === 0) {
+        await connection.rollback(); // Deshacer la transacción en caso de error
+        return res.status(404).json({ message: "No se encontró el indicador", ok: false });
+      }
+  
+      // Confirmar la transacción si todo fue exitoso
+      await connection.commit();
+  
+      res.status(200).json({ message: "Indicador y relaciones eliminados correctamente", ok: true });
+    } catch (error) {
+      console.error('Error al eliminar el indicador y las relaciones:', error);
+      if (connection) await connection.rollback(); // Deshacer la transacción en caso de error
+      res.status(500).json({ message: error.message || "Algo salió mal :(" });
+    } finally {
+      // Cerrar la conexión a la base de datos
+      if (connection) {
+        await connection.end();
+      }
+    }
+  };
+  
+  
+
+  module.exports ={obtenerReparticionesGED,agregarReparticionGED,editarReparticionGED,eliminarReparticionGED,obtenerProgramasGED,agregarProgramaGED,editarProgramaGED,eliminarProgramaGED,obtenerIndicadoresGED,agregarIndicadorGED,editarIndicadorGED,eliminarIndicadorGED,obtenerUnidadesGED,obtenerPerfilPorCuilGED,obtenerReparticionesFiltradasGED,obtenerProgramasPorReparticionGED}
--- routes/gerenciaDatosRoutes.js
+++ routes/gerenciaDatosRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
-const { obtenerReparticiones, eliminarReparticion, obtenerReparticionesGED, agregarReparticionGED, editarReparticionGED, eliminarReparticionGED, obtenerProgramasGED, agregarProgramaGED, editarProgramaGED, eliminarProgramaGED } = require("../controllers/gerenciaDatosControllers");
+const { obtenerReparticiones, eliminarReparticion, obtenerReparticionesGED, agregarReparticionGED, editarReparticionGED, eliminarReparticionGED, obtenerProgramasGED, agregarProgramaGED, editarProgramaGED, eliminarProgramaGED, obtenerIndicadoresGED, agregarIndicadorGED, editarIndicadorGED, eliminarIndicadorGED, obtenerUnidadesGED, obtenerPerfilPorCuilGED, obtenerReparticionesFiltradasGED, obtenerProgramasPorReparticionGED } = require("../controllers/gerenciaDatosControllers");
 const { agregarReparticion, editarReparticion } = require("../controllers/adminControllers");
 
 const router = Router();
 
-
+router.post('/perfil/:cuil', obtenerPerfilPorCuilGED);
 ////////////REPARTICIONES///////////////
 
 router.get("/reparticiones/listar", obtenerReparticionesGED);
+router.post("/reparticiones/listar/:cuil", obtenerReparticionesFiltradasGED);
 router.post("/reparticiones/agregar", agregarReparticionGED);
 router.put("/reparticiones/editar", editarReparticionGED);
 router.delete("/reparticiones/eliminar/:reparticion_id", eliminarReparticionGED);
@@ -15,10 +16,18 @@ router.delete("/reparticiones/eliminar/:reparticion_id", eliminarReparticionGED)
 //////////PROGRAMAS /////////////////////
 
 router.get("/programas/listar", obtenerProgramasGED);
+router.post("/programas/listar/:reparticion_id", obtenerProgramasPorReparticionGED);
 router.post("/programas/agregar", agregarProgramaGED);
 router.put("/programas/editar", editarProgramaGED);
 router.delete("/programas/eliminar/:programa_id", eliminarProgramaGED);
 
+
+////////////INDICADORES/////////////////////////
+router.get("/indicadores/listar", obtenerIndicadoresGED);
+router.get("/unidades/listar", obtenerUnidadesGED);
+router.post("/indicadores/agregar", agregarIndicadorGED);
+router.put("/indicadores/editar", editarIndicadorGED);
+router.delete("/indicadores/eliminar/:indicador_id", eliminarIndicadorGED);
 
 
 module.exports = router;
